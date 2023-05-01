17435113193ef70755c9c315b0dc1975dd028793
Leonardo Angelucci <mail@leonardo-angelucci.ch>
2023-05-01 16:43:19 +0200

    change base 6

--- Webcam and CCTV cameras/P5 BASE - 6/sketch.js
+++ Webcam and CCTV cameras/P5 BASE - 6/sketch.js
@@ -1,68 +1,50 @@
 /*
-In this I want to show you how an element can move according to the frame rate. 
-then by itself.
-In relation to the keypress function we can change the direction.
+Mousemove and mouse click
+In this case, the shape follows the movement of the mouse.
 */
 
-let direction = false;
-let speed = 5;
-let dimension = 40;
-let currentPositionX = 0;
-let currentPositionY = 0;
-//let str = "";
+let click = false;
+let img1, img2;
+let img_width = 100;
+
+function preload() {
+  img1 = loadImage("1.jpg");
+  img2 = loadImage("2.jpg");
+}
 
 function setup() {
   createCanvas(400, 400);
   background(200, 200, 200);
-  currentPositionY = height / 2 - dimension / 2;
 }
 
 function draw() {
-  background(200, 200, 200);
-
-  /*
-  If the direction is false, we move our rectangle horizontally.
-  */
-  if (direction == false) {
-    fill(255, 0, 0);
-    noStroke();
-
-    /*
-    The draw function is an infinite loop.
-    So we can increment each variable.
-    */
-    currentPositionX = currentPositionX + speed;
-    rect(currentPositionX, currentPositionY, dimension, dimension);
-
-    /*
-    We change the direction of the rectangle 
-    if we have reached the end or are at the beginning.
-    if current position is grather than width it come back
-    */
-
-    if (currentPositionX > width - dimension || currentPositionX < 0) {
-      speed = speed * -1;
-    }
+  if (click == false) {
+    background(200, 0, 200);
+    imageMode(CENTER);
+    image(
+      img1,
+      mouseX,
+      mouseY,
+      (img1.width / img1.height) * img_width,
+      img_width
+    );
   } else {
-    fill(0, 255, 0);
-    noStroke();
-    currentPositionY = currentPositionY + speed;
-    rect(currentPositionX, currentPositionY, dimension, dimension);
-    if (currentPositionY > height - dimension || currentPositionY < 0) {
-      speed = speed * -1;
-    }
+    background(0, 0, 255);
+    imageMode(CENTER);
+    image(
+      img2,
+      mouseX,
+      mouseY,
+      (img2.width / img2.height) * img_width,
+      img_width
+    );
   }
 }
 
-/*
-With this function we understand which letter was clicked on the keyboard. 
-Depending on what we click on, we can change the value of a variable.
-*/
-function keyTyped() {
-  str = key;
-  if (key === "a") {
-    direction = false;
+function mouseClicked() {
+  if (click == false) {
+    click = true;
   } else {
-    direction = true;
+    click = false;
   }
 }
